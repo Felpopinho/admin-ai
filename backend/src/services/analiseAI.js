f87a4a17e1b2aea3031dcda8fce8c8e8d06e2fb3
Felpopinho <felipetomaz2609@gmail.com>
2025-10-24 23:02:54 -0300

    refactor: update Firebase configuration to use environment variables

--- backend/src/services/analiseAI.js
+++ backend/src/services/analiseAI.js
@@ -2,13 +2,13 @@ import { getAI, getGenerativeModel, GoogleAIBackend } from "firebase/ai";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNITA081TuIkFvHsd_zsH0aOPtzO8BUGk",
-  authDomain: "admin-ai-fd783.firebaseapp.com",
-  projectId: "admin-ai-fd783",
-  storageBucket: "admin-ai-fd783.firebasestorage.app",
-  messagingSenderId: "338940027519",
-  appId: "1:338940027519:web:b012705e0fd8e49f54954a",
-  measurementId: "G-D1PZR10YB2"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
